0e5dd5b4dab5a297647a46ea38a6d2734f16b411
Daniel Villar González <69615239+danielvillarg@users.noreply.github.com>
2024-03-14 18:26:42 -0500

    Listo para el release

--- parcial1/src/index.js
+++ parcial1/src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-https://gist.githubusercontent.com/josejbocanegra/0067d2b28b009140fee423cfc84e40e6/raw/6e6b11160fbcacb56621b6422684d615dc3a0d33/spaces.json
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
